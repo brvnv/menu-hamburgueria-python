e04cc54cbce1e2efe947e528a73dfd99cd2a866d
brvnv <bruno.rdx@metalfan.com>
2021-05-07 22:45:23 -0300

    Commit inicial

---
+++ menu.py
@@ -0,0 +1,26 @@
+sanduiche1 {
+    "nome": "Heavy Metal",
+    "pao": "Brioche",
+    "molho": "Maionese da casa",
+    "recheio": "Hambúrguer, ovo, alface, tomate, presunto, queijo, bacon, purê de batata, milho verde",
+    "preco": 15.00,
+    "vegano": False
+}
+
+sanduiche2 {
+    "nome": "Lounge",
+    "pao": "Australiano",
+    "molho": "Barbecue e maionese da casa",
+    "recheio": "Hambúrguer, queijo, alface, tomate, milho verde",
+    "preco": 10.00,
+    "vegano": False
+}
+
+sanduiche3 {
+    "nome": "Soul",
+    "pao": "Ciabatta",
+    "molho": "Mostarda agridoce",
+    "recheio": "Hambúrguer de soja e abobrinha, alface, tomate, purê de batata, milho verde, cebola roxa",
+    "preco": 15.00,
+    "vegano": True
+}
